d7511331b47d507c95e3067d00adbbec4315213b
CCC0203 <13840209836@163.com>
2020-09-21 07:58:15 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login-container">
+    <el-form ref="loginForm" >
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
